9dc66a01dd63f93132d8ae3dd2b52e4594ef1728
sahal khasim <sahalkahsim4@gmail.com>
2021-06-03 22:41:01 +0530

    static

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('', include('app1.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
